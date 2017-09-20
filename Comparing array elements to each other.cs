97e24a686143dca7efe263500e208aa6486fe075
VPapazyan <papazyanvahram.vp@gmail.com>
2017-09-20 12:54:49 +0400

    Add files via upload

---
+++ Comparing array elements to each other.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 10, 20, 30, 10, 50 };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i+1; j < array.Length-i; j++)
+                {
+                    if(array[i] == array[j])
+                    {
+                        
+                        Console.WriteLine($"array[{i}] and array[{j}] are identical");
+                        break;
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
