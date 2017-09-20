a05853a74bc2fbdce00142baae12426521006244
VPapazyan <papazyanvahram.vp@gmail.com>
2017-09-20 13:14:23 +0400

    Add files via upload

---
+++ The average of an array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 10, 20, 30, 40, 50 };
+            int average =0;
+
+            for (int i = 0; i < array.Length; i++)
+            {                
+                average += array[i];
+            }
+            average /= array.Length;
+
+            Console.WriteLine(average);
+            Console.ReadLine();
+        }
+    }
+}
