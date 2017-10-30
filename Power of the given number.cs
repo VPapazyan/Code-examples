2aaaf372f4c849d29398e49d529864ad06c4950c
Vahram <papazyanvahram.vp@gmail.com>
2017-10-30 21:03:48 +0400

    Add files via upload

---
+++ Power of the given number.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Power_of_the_given_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int result = 1;
+
+            for (int i = 1; i <= b; i++)
+            {
+                result *= a;
+            }
+            Console.WriteLine($"The result is {result}");
+            Console.ReadKey();
+        }
+    }
+}
