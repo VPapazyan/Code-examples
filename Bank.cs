539825c4b931db479e5158cd8ad15ea29ca40b76
Vahram <papazyanvahram.vp@gmail.com>
2017-10-31 00:10:52 +0400

    Add files via upload

---
+++ Bank.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_lecture_1
+{
+    public class Bank
+    {
+        public string Name = "Ameria";
+        public string Address = "Moskovyan 8";
+        public int EmployeeCount = 90;
+
+        public void IVR()
+        {
+            string response = $"Hello dear customer. You called {Name} bank. Our address is {Address}";
+            Console.WriteLine(response);
+        }
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_lecture_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank Ameria = new Bank();
+            Console.WriteLine("The name of the bank is {0}", Ameria.Name);
+            Ameria.IVR();
+
+
+            Console.Read();
+        }
+    }
+}
