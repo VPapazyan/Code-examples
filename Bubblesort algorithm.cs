382ed0fdc5f3bcf4cc1f565fd28f87c11efa0f8e
Vahram <papazyanvahram.vp@gmail.com>
2017-10-30 22:18:02 +0400

    Add files via upload

---
+++ Bubblesort algorithm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Bubblesort_algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 6, 5, 4, 7, 1, 3 };
+            int emptyVar;
+
+            Console.Write("Sorted array: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        emptyVar = array[i];
+                        array[i] = array[j];
+                        array[j] = emptyVar;
+                    }
+                }
+                
+                Console.Write(array[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
