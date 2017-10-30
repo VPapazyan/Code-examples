5d4770a21b809848c663536a599ba9af846dad91
Vahram <papazyanvahram.vp@gmail.com>
2017-10-30 21:20:33 +0400

    Add files via upload

---
+++ Tha factorial of a given number.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Factorial of a given number
+            int num = int.Parse(Console.ReadLine());
+                       
+            Console.WriteLine($"The result is {Factorial(num)}");
+            Console.ReadLine();
+        }
+
+        static int Factorial(int num)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
