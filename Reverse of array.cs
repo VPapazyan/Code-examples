cff90b42f83ee724cbd3b0469e586b4ee0d92c8f
Vahram <papazyanvahram.vp@gmail.com>
2017-10-30 23:04:16 +0400

    Add files via upload

---
+++ Reverse of array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Reverse_of_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6 };
+            int temp = 0;
+
+            Console.Write("Reversed array: ");
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                temp = array[i];
+                array[i] = array[array.Length - i - 1];
+                array[array.Length - i - 1] = temp;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
